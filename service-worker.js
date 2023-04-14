458daca6f88c29e440e3819306f7350d8e35b219
Boora Hruday Abhinav Manohar <105707241+Abhinav007700@users.noreply.github.com>
2023-04-14 21:23:54 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,35 @@
-const cacheName= 'test-cache';
-const filesToCache=[
-    '/',
-    '/index.html',
-    '/index.css',
-    '/index.js'
+const cacheName = 'test-cache';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/index.css',
+  '/index.js'
 ];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(cacheName).then((cache) => {
-            return cache.addAll(filesToCache);
-        })
-    );
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
+
 self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((keyList) => {
-            return Promise.all(keyList.map((key) => {
-                if (key !== cacheName) {
-                    return caches.delete(key);
-                }
-            }));
-        })
-    );
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
