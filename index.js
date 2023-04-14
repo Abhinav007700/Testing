b4b22c5633bf13fff9eb1a6c35f0758975a1b5d9
Boora Hruday Abhinav Manohar <105707241+Abhinav007700@users.noreply.github.com>
2023-04-14 21:12:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function display(){
+    document.getElementById('t1').innerHTML = 'This is a sample test test.';
+}
+function remove(){
+    document.getElementById('t1').innerHTML = '';
+}
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const cacheName= 'test-cache';
+const filesToCache=[
+    '/',
+    '/index.html',
+    '/index.css',
+    '/index.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== cacheName) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
